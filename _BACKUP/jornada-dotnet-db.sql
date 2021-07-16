69835b466588e3b49b6197fc02fd70ef6fc07277
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-07-16 00:00:00 -0300

    CMD: dotnet user-secrets init/set/list

---
+++ _BACKUP/jornada-dotnet-db.sql
@@ -0,0 +1,110 @@
+USE [master]
+GO
+
+/****** Object:  Database [jornada-dotnet-db]    Script Date: 13/10/2021 13:53:56 ******/
+CREATE DATABASE [jornada-dotnet-db]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'jornada-dotnet-db', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\jornada-dotnet-db.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'jornada-dotnet-db_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\jornada-dotnet-db_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET COMPATIBILITY_LEVEL = 120
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [jornada-dotnet-db].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [jornada-dotnet-db] SET  READ_WRITE 
+GO
