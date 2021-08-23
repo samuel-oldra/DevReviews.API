cb666f782f321c7f3063b4a3ecc37ccbe4325bb9
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-08-23 19:00:00 -0300

    Fix: Configurando banco de dados (DbContext)

--- DevReviews.API/Startup.cs
+++ DevReviews.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 using DevReviews.API.Persistence;
 using DevReviews.API.Profiles;
