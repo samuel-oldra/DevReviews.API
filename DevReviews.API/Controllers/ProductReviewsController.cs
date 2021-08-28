762249e4fc3824188e7fcb43e616d18b9b23f98e
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-08-28 19:00:00 -0300

    Persistindo dados dos reviews

--- DevReviews.API/Controllers/ProductReviewsController.cs
+++ DevReviews.API/Controllers/ProductReviewsController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using DevReviews.API.Entities;
 using DevReviews.API.Models;
+using DevReviews.API.Persistence;
 
 namespace DevReviews.API.Controllers
 {
@@ -12,13 +15,31 @@ namespace DevReviews.API.Controllers
     [Route("api/products/{productId}/productreviews")]
     public class ProductReviewsController : ControllerBase
     {
+        private readonly DevReviewsDbContext _dbContext;
+
+        private readonly IMapper _mapper;
+
+        public ProductReviewsController(DevReviewsDbContext dbContext, IMapper mapper)
+        {
+            this._dbContext = dbContext;
+            this._mapper = mapper;
+        }
+
         // GET: api/products/1/productreviews/5
         [HttpGet("{id}")]
         public IActionResult GetById(int productId, int id)
         {
             // Se não existir com o id especificado, retornar NotFound()
+            var productReview = _dbContext.ProductReviews.SingleOrDefault(p => p.Id == id);
 
-            return Ok();
+            if (productReview == null)
+            {
+                return NotFound();
+            }
+
+            var productDetails = _mapper.Map<ProductReviewDetailsViewModel>(productReview);
+
+            return Ok(productDetails);
         }
 
         // POST: api/products/1/productreviews
@@ -26,8 +47,12 @@ namespace DevReviews.API.Controllers
         public IActionResult Post(int productId, AddProductReviewInputModel model)
         {
             // Se tiver com dados inválidos, retornar BadRequest()
+            var productReview = new ProductReview(model.Author, model.Rating, model.Comments, productId);
 
-            return CreatedAtAction(nameof(GetById), new { id = 1, productId = 2 }, model);
+            _dbContext.ProductReviews.Add(productReview);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = productReview.Id, productId = productId }, model);
         }
     }
 }
