5cb818ee126a10a0cac4808507a4d1b985811a63
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-09-04 00:00:00 -0300

    Test Product UpdateAsync #3

--- DevReviews.API/Tests/ProductTest.cs
+++ DevReviews.API/Tests/ProductTest.cs
@@ -13,7 +13,7 @@ namespace DevReviews.API.Tests
     public class ProductTest
     {
         [Fact]
-        public async Task AddAsync()
+        public async void AddAsync()
         {
             // Arrange
             var addProductInputModel = new Fixture().Create<AddProductInputModel>();
@@ -35,6 +35,33 @@ namespace DevReviews.API.Tests
             addedProduct.Price.ShouldBe(addProductInputModel.Price);
 
             productRepositoryMock.Verify(pr => pr.AddAsync(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public async void UpdateAsync()
+        {
+            // Arrange
+            var addProductInputModel = new Fixture().Create<AddProductInputModel>();
+            var updateProductInputModel = new Fixture().Create<UpdateProductInputModel>();
+
+            var productRepositoryMock = new Mock<IProductRepository>();
+
+            var productService = new ProductService(productRepositoryMock.Object);
+
+            // Act
+            var addedProduct = await productService.AddAsync(addProductInputModel);
+            var updatedProduct = await productService.UpdateAsync(addedProduct, updateProductInputModel);
+
+            // Assert
+            Assert.Equal(updatedProduct.Title, addProductInputModel.Title);
+            Assert.Equal(updatedProduct.Description, updateProductInputModel.Description);
+            Assert.Equal(updatedProduct.Price, updateProductInputModel.Price);
+
+            updatedProduct.Title.ShouldBe(addProductInputModel.Title);
+            updatedProduct.Description.ShouldBe(updateProductInputModel.Description);
+            updatedProduct.Price.ShouldBe(updateProductInputModel.Price);
+
+            productRepositoryMock.Verify(pr => pr.UpdateAsync(It.IsAny<Product>()), Times.Once);
         }
     }
 }
