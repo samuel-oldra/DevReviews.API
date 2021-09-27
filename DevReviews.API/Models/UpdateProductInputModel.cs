bd34f4af53daccd70b6ba3c3de954359de84e951
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-09-27 15:28:13 -0300

    Adicionado input model para reviews

--- DevReviews.API/Models/UpdateProductInputModel.cs
+++ DevReviews.API/Models/UpdateProductInputModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace DevReviews.API.Models
 {
     public class UpdateProductInputModel
-    {   
+    {
         public string Description { get; set; }
 
         public decimal Price { get; set; }
