f802e0e9ff4bbc917e5a47258b2e8e7a2ea6e4bc
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-09-30 19:00:00 -0300

    Adicionado Log com Serilog para SQLite

--- DevReviews.API/Program.cs
+++ DevReviews.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
 
 namespace DevReviews.API
 {
@@ -17,14 +18,8 @@ namespace DevReviews.API
 
                     Serilog.Log.Logger = new LoggerConfiguration()
 
-                        // PARA LOG NO SQL Server
-                        //.WriteTo.MSSqlServer(
-                        //    settings.GetValue<string>("DevReviewsCn"),
-                        //    sinkOptions: new MSSqlServerSinkOptions()
-                        //    {
-                        //        TableName = "Logs",
-                        //        AutoCreateSqlTable = true
-                        //    })
+                        // PARA LOG NO SQlite
+                        .WriteTo.SQLite(Environment.CurrentDirectory + @"\Data\dados.db")
 
                         // PARA LOG NO CONSOLE
                         .WriteTo.Console()
